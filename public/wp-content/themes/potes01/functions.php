8a96a7cc72a23cd9002539a590b05523654feb92
Maarten de Keizer <github@maartendekeizer.nl>
2026-03-16 21:17:55 +0100

    Add polyfill for scroll animation for Firefox

--- public/wp-content/themes/potes01/functions.php
+++ public/wp-content/themes/potes01/functions.php
@@ -1,5 +1,7 @@
 <?php
 
 add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_script('potes01-scroll-timeline', get_theme_file_uri('assets/vendor/scroll-timeline.js'));
     wp_enqueue_style('potes01', get_stylesheet_uri());
+
 });
