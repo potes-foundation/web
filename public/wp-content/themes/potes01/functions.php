d9139076b80a48aa7457aa95de44e3a9574c48d3
Maarten de Keizer <github@maartendekeizer.nl>
2026-03-23 11:12:54 +0100

    Add multi language support

--- public/wp-content/themes/potes01/functions.php
+++ public/wp-content/themes/potes01/functions.php
@@ -3,5 +3,14 @@
 add_action('wp_enqueue_scripts', function () {
     wp_enqueue_script('potes01-scroll-timeline', get_theme_file_uri('assets/vendor/scroll-timeline.js'));
     wp_enqueue_style('potes01', get_stylesheet_uri());
+});
 
+add_filter('body_class', function ($classes) {
+    global $post;
+    $classes[] = 'page-slug--' . sanitize_html_class($post->post_name);
+    if ($post->post_parent) {
+        $parent = get_post($post->post_parent);
+        $classes[] = 'parent-page-slug--' . sanitize_html_class($parent->post_name);
+    }
+    return $classes;
 });
